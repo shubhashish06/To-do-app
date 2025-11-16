a20e8d503ab93950b57f68bad075055985061adc
Shubh Ashish <shubhashish06@gmail.com>
2025-11-16 14:49:25 +0530

    added database handler class which includes the read and write functions for todos

--- rptodo/database.py
+++ rptodo/database.py
@@ -1,14 +1,16 @@
 import configparser
 from pathlib import Path
+import json
+from typing import Any,Dict,List,NamedTuple 
 
-from rptodo import DB_WRITE_ERROR,SUCCESS
+from rptodo import DB_WRITE_ERROR, SUCCESS,DB_READ_ERROR,JSON_ERROR
 
 DEF_DB_FILE_PATH=Path.home().joinpath(
     "."+Path.home().stem+"_todo.json"
 )
 
 def get_database_path(config_file:str)->Path:
-    config_parser=config_parser.ConfigParser()
+    config_parser=configparser.ConfigParser()
     config_parser.read(config_file)
     return Path(config_parser["General"]["database"])
 
@@ -18,3 +20,34 @@ def init_database(db_path:Path)->int:
         return SUCCESS
     except OSError:
         return DB_WRITE_ERROR
+    
+
+class DBResponse(NamedTuple):
+    todo_list:List[Dict[str,Any]]
+    error:int
+
+class DatabaseHandler:
+    def __init__(self,db_path:Path)->None:
+        self._db_path=db_path
+
+    def read_todos(self)->DBResponse:
+        try:
+            with self._db_path.open("r") as db:
+                try:
+                    return DBResponse(json.load(db),SUCCESS)
+                except json.JSONDecodeError:
+                    return DBResponse([],JSON_ERROR)
+                
+        except OSError:
+            return DBResponse([],DB_READ_ERROR)
+        
+    
+    def write_todos(self,todo_list:List[Dict[str,Any]])->DBResponse:
+        try:
+            with self._db_path.open("w") as db:
+                json.dump(todo_list,db,indent=4)
+                return DBResponse(todo_list,SUCCESS)
+            
+        except OSError:
+            return DBResponse(todo_list,DB_WRITE_ERROR)
+
