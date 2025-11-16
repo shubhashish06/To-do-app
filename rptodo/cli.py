b3d726132dac45847ac7520521eeed9c3a95e33f
Shubh Ashish <shubhashish06@gmail.com>
2025-11-16 14:50:49 +0530

    added function get_todoer that gets the config & db file and validates it.

--- rptodo/cli.py
+++ rptodo/cli.py
@@ -1,6 +1,6 @@
-from typing import Optional
+from typing import Optional, List
 import typer
-from rptodo import __app_name__, __version__, ERRORS, config, database
+from rptodo import __app_name__, __version__, ERRORS, config, database, rptodo
 from pathlib import Path
 app = typer.Typer()
 @app.command()
@@ -30,6 +30,21 @@ def init(
         typer.secho(f"The to-do database is {db_path}", fg=typer.colors.GREEN)
 
 
+def get_todoer()->rptodo.Todoer:
+    if config.CONFIG_FILE_PATH.exists():
+        db_path=database.get_database_path(config.CONFIG_FILE_PATH)
+    else:
+        typer.secho(
+            'Config file not found. Please, run "rptodo init"',
+            fg=typer.colors.RED,
+        )
+        raise typer.Exit(1)
+    if db_path.exists():
+        return rptodo.Todoer(db_path)
+    else:
+        typer.secho('Database not found. Please run "rptodo init"',
+                    fg=typer.colors.RED)
+        raise typer.Exit(1)
 
 
 def _version_callback(value:bool)->None:
