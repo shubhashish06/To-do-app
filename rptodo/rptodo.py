e9dc80cd747597143b773cf1b166109b58730301
Shubh Ashish <shubhashish06@gmail.com>
2025-11-20 08:23:32 +0530

    added function add() that adds new to-do in the db

--- rptodo/rptodo.py
+++ rptodo/rptodo.py
@@ -1,4 +1,5 @@
-from typing import Any,Dict,NamedTuple
+from typing import Any,Dict,NamedTuple,List
+from rptodo import DB_READ_ERROR
 from rptodo.database import DatabaseHandler
 from pathlib import Path
 
@@ -10,3 +11,21 @@ class CurrentTodo(NamedTuple):
 class Todoer:
     def __init__(self,db_path:Path)->None:
         self._db_handler=DatabaseHandler(db_path)
+
+    def add(self,description:List[str],priority:int=2)->CurrentTodo:
+        """Add a new to do database"""
+        description_text=" ".join(description)
+        if not description_text.endswith("."):
+            description_text+="."
+        todo={
+            "Description":description_text,
+            "Priority":priority,
+            "Done":False,
+        }
+        read=self._db_handler.read_todos()
+        if read.error==DB_READ_ERROR:
+            return CurrentTodo(todo,read.error)
+        read.todo_list.append(todo)
+        write=self._db_handler.write_todos(read.todo_list)
+        return CurrentTodo(todo,write.error)
+
