d36da349afdca69aff9c94f06bdf9018b56de555
Shubh Ashish <shubhashish06@gmail.com>
2025-11-20 08:22:46 +0530

    added a function test_add that tests the added mock json file

--- tests/test_rptodo.py
+++ tests/test_rptodo.py
@@ -1,8 +1,9 @@
 #tests/test_rptodo.py
 
 from typer.testing import CliRunner
-
-from rptodo import __version__,__app_name__,cli
+import json
+import pytest
+from rptodo import ( DB_READ_ERROR,SUCCESS,__app_name__,__version__,cli,rptodo)
 
 runner=CliRunner()
 
@@ -12,3 +13,53 @@ def test_version():
     assert result.exit_code==0
     assert f"{__app_name__} v{__version__}\n" in result.stdout
 
+
+@pytest.fixture
+def mock_json_file(tmp_path):
+    todo = [{"Description": "Get some milk.", "Priority": 2, "Done": False}]
+    db_file = tmp_path / "todo.json"
+    with db_file.open("w") as db:
+        json.dump(todo, db, indent=4)
+    return db_file
+
+
+test_data1={
+    "description":["Clean","the","house"],
+    "priority":1,
+    "todo":{
+        "Description":"Clean the house.",
+        "Priority":1,
+        "Done":False,
+    },
+}
+
+test_data2={
+    "description":["Wash the car"],
+    "priority":2,
+    "todo":{
+        "Description":"Wash the car.",
+        "Priority":2,
+        "Done":False,
+    },
+}
+
+@pytest.mark.parametrize(
+    "description, priority, expected",
+    [
+        pytest.param(
+            test_data1["description"],
+            test_data1["priority"],
+            (test_data1["todo"], SUCCESS),
+        ),
+        pytest.param(
+            test_data2["description"],
+            test_data2["priority"],
+            (test_data2["todo"], SUCCESS),
+        ),
+    ],
+)
+def test_add(mock_json_file, description, priority, expected):
+    todoer = rptodo.Todoer(mock_json_file)
+    assert todoer.add(description, priority) == expected
+    read = todoer._db_handler.read_todos()
+    assert len(read.todo_list) == 2
